eacc1708006debdfc1f76bd8e108ca5370fb9ce9
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-02 19:20:08 -0300

    getting data

--- cfgames.py
+++ cfgames.py
@@ -2,3 +2,19 @@
 ###### Web Scrapping - Crossfit Games 2022 - Data Analysis 
 ###
 
+# Prototypes
+import pandas as pd
+
+url = 'https://en.wikipedia.org/wiki/CrossFit_Games'
+
+html_cross = pd.read_html(url)
+
+recentData = html_cross[0]
+championsByYear = html_cross[1]
+mastersMens = html_cross[2]
+mastersWeman = html_cross[3]
+teens  = html_cross[4]
+
+print(championsByYear)
+
+
