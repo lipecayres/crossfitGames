b9389b49bd017ce91324a36888af1abb18c13848
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-04 10:25:46 -0300

    pandas-exporting to csv

--- cfgames.py
+++ cfgames.py
@@ -5,6 +5,7 @@
 # Prototypes
 import pandas as pd
 
+# Site URL
 url = 'https://en.wikipedia.org/wiki/CrossFit_Games'
 
 html_cross = pd.read_html(url)
@@ -15,6 +16,12 @@ mastersMens = html_cross[2]
 mastersWeman = html_cross[3]
 teens  = html_cross[4]
 
-print(championsByYear)
+# create DataFrame
 
+df1 = pd.DataFrame(championsByYear)
+
+print(df1)
+# Exporting to csv file
+
+df1.to_csv(r'cross_recentData.csv', index = False)
 
